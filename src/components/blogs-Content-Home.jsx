7ed2c825d77a4ff6bc358928abc002f88218df14
Saeed Mohamed <sydalshghty15@gmail.com>
2025-07-31 13:18:06 +0300

    upload-changes

--- src/components/blogs-Content-Home.jsx
+++ src/components/blogs-Content-Home.jsx
@@ -5,28 +5,44 @@ import blogsline from "../assets/blogs-Line 22.svg";
 import { useNavigate } from "react-router-dom";
 import WebDeveloper from "./web-developer";
 import "../css/blogs-content-home.css";
+import { useInView } from "react-intersection-observer";
+import { motion } from "framer-motion";
+
 function BlogsContentHome(){
+    const { ref, inView } = useInView({ triggerOnce: true });
+    const [titleRef, titleInView] = useInView({ triggerOnce: true });
+
     const navigate = useNavigate("");
     const navigateBlogs = () => {
         navigate("/Blogs");
     }
     return(
-        <div className="blogs-departament">
-            <img className="img-background" src={imgBlogs} alt="blogs-background"/>
-            <div className="container">
-                <div className="col-blogs">
-                    <img className="img-scroll" src={imgScroll} alt="img-blogs"/>
-                    <div className="blogs-title">
-                        <img className="img-title" src={blogsTitle} alt="blogs-Title"/>
-                        <img className="img-line" src={blogsline} alt=""/>
-                        <p>My thoughts on technology and business, welcome to subscribe</p>
+        <div className="relative flex items-center justify-center w-full overflow-hidden blogs-departament">
+            <img className="absolute top-0 left-0 w-full h-full img-background" src={imgBlogs} alt="blogs-background"/>
+            <div className="container z-50 w-full h-full pt-16 pb-16 mt-16">
+                <motion.div className="flex flex-col items-center justify-center col-blogs"
+                    ref={ref}
+                    initial={{ opacity: 0, y: 80 }}
+                    animate={inView ? { opacity: 1, y: 0 } : {}}
+                    transition={{ duration: 1, delay: 0.5 }}
+                >
+                    <img className="mb-5 img-scroll" src={imgScroll} alt="img-blogs"/>
+                    <div className="flex flex-col items-center justify-center blogs-title">
+                        <img className="mb-2 img-title" src={blogsTitle} alt="blogs-Title"/>
+                        <img className="mb-2 img-line" src={blogsline} alt=""/>
+                        <p className="text-white">My thoughts on technology and business, welcome to subscribe</p>
                     </div>
-                </div>
+                </motion.div>
                 <WebDeveloper/>
-                <div className="view-subscribe-buttons">
-                    <button className="viewMore-btn" onClick={navigateBlogs}>View More</button>
-                    <button className="subscribe-btn" onClick={navigateBlogs}>Subscribe</button>
-                </div>
+                <motion.div className="flex justify-center w-full view-subscribe-buttons"
+                    ref={titleRef}
+                    initial={{ opacity: 0, y: 80 }}
+                    animate={titleInView ? { opacity: 1, y: 0 } : {}}
+                    transition={{ duration: 1, delay: 1.1 }}
+                >
+                    <button className="relative w-40 pt-4 pb-4 pl-8 pr-8 mr-10 text-base border-none rounded-full cursor-pointer viewMore-btn h-14 font-extralight bg-brandcolor1 text-bg1color" onClick={navigateBlogs}>View More</button>
+                    <button className="relative w-40 pt-4 pb-4 pl-8 pr-8 text-base rounded-full cursor-pointer subscribe-btn h-14 font-extralight" onClick={navigateBlogs}>Subscribe</button>
+                </motion.div>
             </div>
         </div>
     )
--- src/components/web-developer.jsx
+++ src/components/web-developer.jsx
@@ -2,50 +2,60 @@ import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import webdeveloperImg from "../assets/web-developer.png";
 import readmoreIcon from "../assets/readmore-Icon.svg";
-import { motion } from "framer-motion";
 import "../css/web-developer.css";
+import { useInView } from "react-intersection-observer";
+import { motion } from "framer-motion";
+
 function WebDeveloper(){
+    const { ref, inView } = useInView({ triggerOnce: true });
     const navigate = useNavigate("");
     const navigateBlogs = () => {
         navigate("/Blogs");
     }
 
     return(
-         <div className="web-developer">
+         <motion.div className="flex items-center justify-center w-full web-developer mt-14 mb-14"
+            ref={ref}
+            initial={{ opacity: 0, y: 80 }}
+            animate={inView ? { opacity: 1, y: 0 } : {}}
+            transition={{ duration: 1, delay: 0.8 }}
+         >
             <div className="alldeveloper-content">
                 <motion.img 
                 initial={{x: -100, opacity: 0}}
                 animate={{x: 0, opacity: 1}}
                 transition={{duration: 0.8, delay: 1}}
-                className="img-webdeveloper" src={webdeveloperImg} alt="web-developer"/>
-                <div className="content-web-developer">
+                className="mr-10 img-webdeveloper w-60 h-60" src={webdeveloperImg} alt="web-developer"/>
+                <div className="pt-6 pb-6 content-web-developer">
                     <motion.h2 
                     initial={{x: 100, opacity: 0}}
                     animate={{x: 0, opacity: 1}}
                     transition={{duration: 0.8, delay: 1}}
+                    className="mb-4 text-2xl font-light text-brandcolor1"
                     >What does it take to become a web developer?</motion.h2>
                     <motion.p
                     initial={{x: 100, opacity: 0}}
                     animate={{x: 0, opacity: 1}}
                     transition={{duration: 0.8, delay: 1.4}}
+                    className="mb-4 text-sm text-white font-extralight"
                     >Web development, also known as website development, encompasses a variety of tasks and processes involved in creating websites for the internet…</motion.p>
-                    <motion.div className="col-readmore"
+                    <motion.div className="flex items-center mb-4 col-readmore"
                     initial={{x: 100, opacity: 0}}
                     animate={{x: 0, opacity: 1}}
                     transition={{duration: 0.8, delay: 1.8}}
                     >
-                        <Link className="link-readmore" to={"/Blogs"}>Read More</Link>
-                        <img src={readmoreIcon} onClick={navigateBlogs} alt="img-readmore"/>
+                        <Link className="mr-2 text-sm link-readmore text-brandcolor1" to={"/Blogs"}>Read More</Link>
+                        <img src={readmoreIcon} onClick={navigateBlogs} alt="img-readmore" className="cursor-pointer"/>
                     </motion.div>
-                    <div className="content-developer">
-                        <motion.div className="developer"
+                    <div className="flex content-developer">
+                        <motion.div className="flex items-center justify-center pt-1 pb-1 pl-2 pr-2 mr-4 developer bg-graycolor rounded-2xl"
                         initial={{y: 60, opacity: 0}}
                         animate={{y: 0, opacity: 1}}
                         transition={{duration: 0.8, delay: 2}}
                         >
-                            <h3>Web Developer</h3>
+                            <h3 className="text-xs text-white font-extralight">Web Developer</h3>
                         </motion.div>
-                        <div className="all-text">
+                        <div className="w-full all-text">
                             <motion.div className="col-text"
                             initial={{y: 80, opacity: 0}}
                             animate={{y: 0, opacity: 1}}
@@ -74,7 +84,7 @@ function WebDeveloper(){
                     </div>
                 </div>
             </div>
-        </div>
+        </motion.div>
     )
 }
 export default WebDeveloper;
