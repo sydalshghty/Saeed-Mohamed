0bfc4f3e8929b975cb89c29599276c96adcf931e
sydalshghty <168644290+sydalshghty@users.noreply.github.com>
2026-01-25 22:25:21 +0200

    update-main.js-changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,10 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-//import { HashRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 createRoot(document.getElementById('root')).render(
-    <App/>
+    <HashRouter basename='/'>
+      <App/>
+    </HashRouter>
 );
 
