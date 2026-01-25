cb16f1046749c73a930028391312340d5eed972c
sydalshghty <168644290+sydalshghty@users.noreply.github.com>
2026-01-25 22:33:02 +0200

    upload-changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 createRoot(document.getElementById('root')).render(
-    <HashRouter basename='/'>
+    <BrowserRouter>
       <App/>
-    </HashRouter>
+    </BrowserRouter>
 );
 
