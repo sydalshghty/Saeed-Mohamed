c542675fdd44904ac405cc7216461bdef709d73d
sydalshghty <168644290+sydalshghty@users.noreply.github.com>
2026-01-26 21:14:06 +0200

    update-path-changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
-      <HashRouter>
+      <BrowserRouter basename='/Saeed-Mohamed/'>
         <App/>
-      </HashRouter>
+      </BrowserRouter>
 );
 
